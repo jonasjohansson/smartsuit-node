4df694decba542b94d4d801dd3ccf9657b760c43
Jonas Johansson <jns.johansson@gmail.com>
2018-09-09 21:31:15 +0200

    unity sender

---
+++ SmartSuitSender.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SmartSuitSender : MonoBehaviour
+{
+    private WebSocket w;
+    public String uri = "ws://127.0.0.1:3000";
+
+    IEnumerator Start()
+    {
+        w = new WebSocket(new Uri(uri));
+        yield return StartCoroutine(w.Connect());
+    }
+
+    private void Update()
+    {
+        TraverseHierarchy(this.gameObject.transform);
+    }
+
+    void TraverseHierarchy(Transform root)
+    {
+        foreach (Transform child in root)
+        {
+            float px = child.transform.position.x;
+            float py = child.transform.position.y;
+            float pz = child.transform.position.z;
+            float rx = child.transform.rotation.x;
+            float ry = child.transform.rotation.y;
+            float rz = child.transform.rotation.z;
+            String msg = "{";
+            msg += "\"name\":\"" + child.name + "\",";
+            msg += "\"px\":" + px + ",";
+            msg += "\"py\":" + py + ",";
+            msg += "\"pz\":" + pz + ",";
+            msg += "\"rx\":" + rx + ",";
+            msg += "\"ry\":" + ry + ",";
+            msg += "\"rz\":" + rz;
+            msg += "}";
+            //Debug.Log(msg);
+            w.SendString(msg);
+            TraverseHierarchy(child);
+        }
+    }
+}
